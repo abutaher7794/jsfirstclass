b2001a682c64cee7798a119dc3b4c859878e07fe
abutaher7794 <abutaher77794@gmail.com>
2024-11-17 09:27:05 +0600

    1

---
+++ index.js
@@ -0,0 +1,72 @@
+// 1. my first js
+console.log("hello js")
+
+// 2. javascript variable
+var num = "Taher ";
+let num1 = "Khan";
+const num2 = num + num1;
+console.log(num2);
+
+// 3.if statement
+let age = 40;
+if (age < 50) {
+  console.log("yes you are adult");
+};
+
+// 4. if else statement
+let momota1 = 50;
+if (momota1 > 50) {
+  console.log("no you are momota")
+}
+else {
+  console.log("yes you are momota")
+};
+
+// 5. if , else if , else statement
+
+let result = 32;
+if (result <= 100 & result >= 80) {
+  console.log("A+")
+}
+else if (result <= 79 & result >= 70) {
+  console.log("A")
+}
+else if (result <= 69 & result >= 60) {
+  console.log("A-")
+}
+else if (result <= 59 & result >= 50) {
+  console.log("B")
+}
+else if (result <= 33 & result >= 40) {
+  console.log("D")
+}
+else {
+  console.log("F")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
